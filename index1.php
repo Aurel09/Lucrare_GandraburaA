f5c47c9e4fb30fa39b0e4fbc87e364e7e6bbf5e0
Aurel09 <45491590+Aurel09@users.noreply.github.com>
2018-12-13 17:49:09 +0300

    Add files via upload

--- index1.php
+++ index1.php
@@ -1,73 +1,141 @@
-
 <?php
 abstract class Autobus
-{
-public function move(){
-return 'Autobuzul Porneste';
-    }
-public function rotateLeft(){
-return 'Facem La Stinga';
-    }
-public function rotateRight(){
-return 'Facem La Dreapta';
-    }
-public function stop(){
-return 'Oprire Autobus';
-    }
 
+    public function MaximumNumberOfPassengers(){
+        return 'Numarul Maxim de pasageri este';
+    }
+    public function move(){
+        return 'Autobuzul Porneste';
+    }
+    public function rotateLeft(){
+        return 'Coteste La Stinga';
+    }
+    public function rotateRight(){
+        return 'Coteste La Dreapta';
+    }
+    public function stop(){
+        return 'Oprire Autobus';
+    }
+    public function fuel(){
+        return 'Combustibil';
+    }
+    public function distance(){
+        return 'parcurge distanta de';
+    }
 }
 
 
 class Car extends Autobus {
 
-public $name = 'Autobuzul';
+    public $name = 'Autobuzul';
 
-public function start()
+    
+  
+    public function distance($z)
     {
-return $this->name.' '. 'Start Ruta';
-    }
-public function stop()
-    {
-return $this->name.' '. 'Stop Ruta';
-    }
-public function move($x)
-    {
-return $this->name .' '.$x;
+        return $this->name . ' ' . parent::distance() .' '.$z;
     }
 
-public function rotateRight()
+    public function fuel($y)
     {
-return $this->name . ' ' . parent::rotateRight();
+        return parent::fuel().' '. 'in rezervor %'.''.$y;
     }
-public function rotateLeft()
+    public function start()
     {
-return $this->name . ' ' . parent::rotateLeft();
+        return $this->name.' '. 'Start Ruta';
+    }
+    public function stop()
+    {
+        return $this->name.' '. 'Stop Ruta';
+    }
+    public function move($x)
+    {
+        return $this->name .' '.$x;
+    }
+
+    public function rotateRight()
+    {
+        return $this->name . ' ' . parent::rotateRight();
+    }
+    public function rotateLeft()
+    {
+        return $this->name . ' ' . parent::rotateLeft();
     }
 
 }
 
-
 $car = new Car();
 echo $car->start();
 ?><hr><?
-echo $car->move('merge spre oporirea nr 1');
+echo $car->fuel(100);
+?><hr><?
+echo $car->move('porneste spre oporirea nr 1');
+?><hr><?
+echo $car->rotateRight();
+?><hr><?
+echo $car->rotateRight();
+?><hr><?
+echo $car->distance(40);
+?><hr><?
+echo $car->rotateLeft();
+?><hr><?
+echo $car->distance(15);
 ?><hr><?
 echo $car->move('stopeaza la oporirea nr 1');
 ?><hr><?
 echo $car->move('porneste la oporirea nr 2');
 ?><hr><?
+echo $car->fuel(90);
+?><hr><?
+echo $car->distance(90);
+?><hr><?
 echo $car->move('stopeaza la oporirea nr 2');
 ?><hr><?
 echo $car->move('porneste la oporirea nr 3');
+?><hr><?
+echo $car->fuel(77);
+?><hr><?
+echo $car->distance(30);
+?><hr><?
+echo $car->rotateLeft();
+?><hr><?
+echo $car->distance(25);
 ?><hr><?
 echo $car->move('stopeaza la oporirea nr 3');
 ?><hr><?
 echo $car->move('porneste la oporirea nr 4');
 ?><hr><?
+echo $car->fuel(60);
+?><hr><?
+echo $car->distance(10);
+?><hr><?
+echo $car->rotateLeft();
+?><hr><?
+echo $car->distance(15);
+?><hr><?
+echo $car->rotateRight();
+?><hr><?
+echo $car->distance(10);
+?><hr><?
+echo $car->rotateLeft();
+?><hr><?
+echo $car->distance(13);
+?><hr><?
 echo $car->move('Stopeaza la oporirea nr 4');
 ?><hr><?
 echo $car->move('porneste la oporirea nr 5');
 ?><hr><?
+echo $car->fuel(40);
+?><hr><?
+echo $car->distance(90);
+?><hr><?
 echo $car->move('Stopeaza la oporirea nr 5');
 ?><hr><?
+echo $car->fuel(30);
+?><hr><?
+echo $car->distance(40);
+?><hr><?
 echo $car->stop();
+?><hr><?
+echo $car->fuel(20);
+?><hr><?
